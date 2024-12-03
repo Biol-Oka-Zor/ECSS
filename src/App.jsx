3c87f74641933b91829cc714dca10ab5647a6b43
VIctus <biloltohirhonov@gmail.com>
2024-12-03 11:36:02 +0500

    ECSS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './styles/main'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div className='box'>
+    <p>Welcome</p>
+
+    <p>Good Afternoon</p>
+    </div>
+       
+    
+  )
+}
+
+export default App
